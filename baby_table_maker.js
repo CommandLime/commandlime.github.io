6de5c044409f5a841506ff644c3080ebdcddbd63
Chris S <9094351+CommandLime@users.noreply.github.com>
2024-10-17 12:58:19 +0100

    stylesheets

---
+++ baby_table_maker.js
@@ -0,0 +1,8 @@
+/* 
+var babyTable = $("#babyTable");
+for (i = 0, i < 20, i++) {
+    babyTable.create(
+        "<td .pos></td>  <td. pot ></td>   <td .club></td> <td .gPl></td> <td .></td>    <td .gW></td>    <td .gD></td>    <td .gL></td>    <td .gF></td>    <td .gA></td>    <td .gDiff></td>    <td .points></td>"
+
+    );
+} */
---
+++ tablesearch.js
@@ -0,0 +1,11 @@
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
